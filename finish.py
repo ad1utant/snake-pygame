8147a6f14eac6ea3a826e4b32673992da587faa4
adiutant <mikolaj@marasek.eu>
2023-02-08 23:56:00 +0100

    add death graphics

--- finish.py
+++ finish.py
@@ -4,6 +4,14 @@ class Finish():
     def __init__(self):
         self.status = 'nonactivated'
     def finish(self,counter,player):
+        if player.direction == 'up':
+            player.surf = pygame.image.load('graphics/up_death.png')
+        elif player.direction == 'down':
+            player.surf = pygame.image.load('graphics/down_death.png')
+        elif player.direction == 'left':
+            player.surf = pygame.image.load('graphics/left_death.png')
+        elif player.direction == 'right':
+            player.surf = pygame.image.load('graphics/right_death.png')
         player.direction = 'finish'
         self.font = pygame.font.Font("fonts/sofia.ttf", 60)
         self.surf = self.font.render(f'Your score is {counter.value}', True, [255, 255, 255], None)
